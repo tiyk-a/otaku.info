c21975c1ae9817c1f059f3d98b902ab4ca78920c
Chiara <milejanaja@gmail.com>
2021-06-28 19:20:05 +0900

    Initial commit

---
+++ src/test/java/otaku/info/InfoApplicationTests.java
@@ -0,0 +1,13 @@
+package otaku.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

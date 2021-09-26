5c9dfb4b3bcb7b497c63e20f64e89457e13dae7f
Chiara <milejanaja@gmail.com>
2021-09-26 23:26:58 +0900

    🧚‍♀️ 複数バッチ同時実行ができるようにする

--- src/main/java/otaku/info/batch/scheduler/SchedulingConfiguration.java
+++ src/main/java/otaku/info/batch/scheduler/SchedulingConfiguration.java
@@ -1,8 +1,10 @@
 package otaku.info.batch.scheduler;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * Handle EnableScheduling depend on the work environment
@@ -12,4 +14,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @ConditionalOnProperty(prefix = "setting", name="enableScheduling", havingValue="true", matchIfMissing = true)
 public class SchedulingConfiguration {
+
+    /**
+     * 複数バッチの同時実行（時間被り）を行えるようにする
+     * https://atl2.net/springboot/scheduled%E3%81%A7%E5%90%8C%E6%99%82%E5%AE%9F%E8%A1%8C%E3%81%99%E3%82%8B/
+     *
+     * @return
+     */
+    @Bean(destroyMethod = "shutdown")
+    ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        return taskScheduler;
+    }
 }

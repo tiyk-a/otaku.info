396567e41cf8ddaf4c8fdc5020b2998d13607016
Chiara <milejanaja@gmail.com>
2021-10-02 04:44:51 +0900

    🧚‍♀️ StatioinEnum added

--- src/main/java/otaku/info/searvice/StationService.java
+++ src/main/java/otaku/info/searvice/StationService.java
@@ -3,9 +3,11 @@ package otaku.info.searvice;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import otaku.info.entity.Station;
+import otaku.info.enums.StationEnum;
 import otaku.info.repository.StationRepository;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Service
@@ -16,13 +18,19 @@ public class StationService {
     private final StationRepository stationRepository;
 
     public Optional<Long> findStationId(String stationName) {
-        return stationRepository.findStationId(stationName);
+        return Arrays.stream(StationEnum.values()).filter(e -> e.getName().equals(stationName)).map(e -> (long) e.getId()).findFirst();
     }
 
     public String getStationName(Long stationId) {
-        return stationRepository.getStationName(stationId);
+        return Arrays.stream(StationEnum.values()).filter(e -> e.getId().equals(Math.toIntExact(stationId))).map(StationEnum::getName).findFirst().orElse("");
     }
 
+    /**
+     * DBに登録します
+     *
+     * @param station
+     * @return
+     */
     public Station save(Station station) {
         return stationRepository.save(station);
     }

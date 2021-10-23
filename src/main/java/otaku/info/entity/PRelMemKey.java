eca6acc9969cba7d9e882c1a858ca3c1e67e39a3
Chiara <milejanaja@gmail.com>
2021-10-23 13:51:11 +0900

    👍 Warning solved

--- src/main/java/otaku/info/entity/PRelMemKey.java
+++ src/main/java/otaku/info/entity/PRelMemKey.java
@@ -1,8 +1,15 @@
 package otaku.info.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import java.io.Serializable;
 
+@Setter
+@Getter
+@Data
 public class PRelMemKey implements Serializable {
 
     @Column(name = "p_rel_mem_id")

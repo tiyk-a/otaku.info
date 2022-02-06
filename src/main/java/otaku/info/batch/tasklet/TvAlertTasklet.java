ff55dc9b927340c1c332a27c76328082203b2d0d
Chiara <milejanaja@gmail.com>
2022-02-06 12:30:09 +0900

    🐛 嵐のtvエラーが多すぎるので抜いた

--- src/main/java/otaku/info/batch/tasklet/TvAlertTasklet.java
+++ src/main/java/otaku/info/batch/tasklet/TvAlertTasklet.java
@@ -10,11 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import otaku.info.controller.PythonController;
 import otaku.info.controller.TwTextController;
+import otaku.info.entity.PRel;
 import otaku.info.entity.Program;
+import otaku.info.enums.TeamEnum;
+import otaku.info.repository.PRelRepository;
 import otaku.info.service.ProgramService;
 import otaku.info.setting.Log4jUtils;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,11 +38,29 @@ public class TvAlertTasklet implements Tasklet {
     @Autowired
     ProgramService programService;
 
+    @Autowired
+    PRelRepository pRelRepository;
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         // これから1時間以内に放送開始し、チームIDも埋まっているレコードを取得する。
         List<Program> programList = programService.findByOnAirDateTimeTeamId(LocalDateTime.now(), 1);
+        logger.debug("*** program list " + programList.size());
         if (programList.size() > 0) {
+            // 嵐のエラーが多いので嵐はちょっと抜くtmp対応
+            List<Long> removeList = new ArrayList<>();
+            for (Program p : programList) {
+                List<PRel> pRelList = pRelRepository.findAllByProgramId(p.getProgram_id());
+                boolean postFlg = pRelList.stream().noneMatch(e -> e.getTeam_id().equals(TeamEnum.get("ARASHI").getId()));
+
+                if (!postFlg) {
+                    removeList.add(p.getProgram_id());
+                }
+            }
+
+            programList.removeIf(e -> removeList.contains(e.getProgram_id()));
+
+            logger.debug("*** post counts " + programList.size());
             // Postする番組の投稿文を作る Map<ProgramId-TeamId, text>
             Map<Long, String> postMap = new HashMap<>();
             programList.forEach(e -> postMap.putAll(twTextController.tvAlert(e)));

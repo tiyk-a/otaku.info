46c3688419ccd5d6af10d457f858c6189f3abaf8
Chiara <milejanaja@gmail.com>
2022-02-16 21:10:14 +0900

    😋 Forgot to add enum

--- src/main/java/otaku/info/enums/GColorEnum.java
+++ src/main/java/otaku/info/enums/GColorEnum.java
@@ -1,2 +1,22 @@
-package otaku.info.enums;public class GoogleColorEnum {
+package otaku.info.enums;
+
+public enum GColorEnum {
+
+    PALE_BLUE("1"),
+    PALE_GREEN("2"),
+    MAUVE("3"),
+    PALE_RED("4"),
+    YELLOW("5"),
+    ORANGE("6"),
+    CYAN("7"),
+    GRAY("8"),
+    BLUE("9"),
+    GREEN("10"),
+    RED("11");
+        
+    private final String id;
+
+    GColorEnum(String id) {
+        this.id = id;
+    }
 }

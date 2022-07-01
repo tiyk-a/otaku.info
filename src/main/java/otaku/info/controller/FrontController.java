38a52a18f695f381b8cbf33a098ccccc062959b1
Chiara <milejanaja@gmail.com>
2022-07-01 20:31:55 +0900

    🎨 Front on Java base implemented

--- src/main/java/otaku/info/controller/FrontController.java
+++ src/main/java/otaku/info/controller/FrontController.java
@@ -1,9 +1,24 @@
 package otaku.info.controller;
 
 import lombok.AllArgsConstructor;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import otaku.info.dto.*;
+import otaku.info.entity.*;
+import otaku.info.setting.Log4jUtils;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Front pageを表示するためのコントローラです
@@ -14,9 +29,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @AllArgsConstructor
 public class FrontController {
 
-    @GetMapping("/tmp")
-    public void profile() {
-        System.out.println("koko");
+    final Logger logger = Log4jUtils.newConsoleCsvAllLogger("FrontController");
+
+    @Autowired
+    ApiController apiController;
+
+    @GetMapping("/item/{id}")
+    public String getItemTeam(@PathVariable("id") Long teamId, Model model) {
+        ResponseEntity<List<Item>> res = apiController.getItemTeam(teamId);
+        model.addAttribute("test", "testdayo");
+        model.addAttribute("itemList", res.getBody());
+        return "item";
+    }
+
+    @GetMapping("/tv/{id}")
+    public String tv(@PathVariable("id") Long teamId, Model model) {
+        model.addAttribute("test", "testdayo");
+        ResponseEntity<PAllDto> res = apiController.tvAll(teamId);
+        model.addAttribute("pAllDto", res.getBody());
+        return "tv";
     }
 
     /**
@@ -24,6 +55,5 @@ public class FrontController {
      */
     @GetMapping("/tw")
     public void getTwitter() {
-//        System.out.println("koko");
     }
 }
